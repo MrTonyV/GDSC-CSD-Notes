b73fde3cc587d495a0acb17eddbd7dbc2f1de792
Stelios Papamichail <steliospapamichail@protonmail.com>
2023-04-18 19:35:22 +0300

    - Implemented navigation & add notes page UI

--- app/src/main/java/com/gdscuoc/csdnotes/ui/navigation/MainNavHost.kt
+++ app/src/main/java/com/gdscuoc/csdnotes/ui/navigation/MainNavHost.kt
@@ -1,7 +1,19 @@
 package com.gdscuoc.csdnotes.ui.navigation
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -9,21 +21,34 @@ import androidx.navigation.compose.rememberNavController
 import com.gdscuoc.csdnotes.ui.screens.AddNotePage
 import com.gdscuoc.csdnotes.ui.utils.Screens
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainNavHost(
     modifier: Modifier = Modifier,
     navController: NavHostController = rememberNavController()
 ) {
-    NavHost(
-        modifier = modifier,
-        navController = navController,
-        startDestination = Screens.AddNotePage.navRoute
-    ) {
-        composable(Screens.AllNotes.navRoute) {
+    var currentScreen by remember {
+        mutableStateOf(Screens.AllNotes.name)
+    }
 
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            CenterAlignedTopAppBar(title = { Text(currentScreen, fontSize = 20.sp) })
         }
-        composable(Screens.AddNotePage.navRoute) {
-            AddNotePage(navController)
+    ) { paddingValues ->
+        NavHost(
+            modifier = modifier.padding(paddingValues),
+            navController = navController,
+            startDestination = Screens.AddNotePage.navRoute
+        ) {
+            composable(Screens.AllNotes.navRoute) {
+                currentScreen = Screens.AllNotes.name
+            }
+            composable(Screens.AddNotePage.navRoute) {
+                AddNotePage(navController)
+                currentScreen = Screens.AddNotePage.name
+            }
         }
     }
 }
--- app/src/main/java/com/gdscuoc/csdnotes/ui/screens/AddNotePage.kt
+++ app/src/main/java/com/gdscuoc/csdnotes/ui/screens/AddNotePage.kt
@@ -12,6 +12,10 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -22,27 +26,29 @@ import androidx.navigation.NavController
 fun AddNotePage(
     navController: NavController
 ) {
-    var title = ""
-    var description = ""
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
 
     Column(
         Modifier
             .fillMaxSize()
-            .padding(horizontal = 16.dp),
+            .padding(horizontal = 16.dp, vertical = 24.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
             label = { Text("Title") },
             value = title, onValueChange = {
                 title = it.trim()
             })
         Spacer(Modifier.height(24.dp))
         OutlinedTextField(
-            label = { Text("description") },
+            modifier = Modifier.fillMaxWidth(),
+            label = { Text("Description") },
             value = description, onValueChange = {
                 description = it.trim()
             })
-        Spacer(Modifier.height(24.dp))
+        Spacer(Modifier.weight(1f))
         Button(modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight(), onClick = { /*TODO*/ }) {
